a700dcff1e1813bc9a8c4159c0e3ccde6def0e8e
zhangyuhang <2657955369@qq.com>
2022-03-10 15:08:42 +0800

    feat:merge from zyhdev

--- app/src/main/java/com/scrm/robot/MainActivity.java
+++ app/src/main/java/com/scrm/robot/MainActivity.java
@@ -4,11 +4,14 @@ import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Messenger;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.scrm.robot.taskmanager.JobSchedulerMessageHandler;
 import com.scrm.robot.taskmanager.JobSchedulerService;
@@ -56,29 +59,122 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        suspensionWindowPermissionCheck();
+    }
+
+    @Override
     protected void onStop(){
         stopService(new Intent(this, JobSchedulerService.class));
         super.onStop();
     }
 
-
-
     public void btnOpenWeWorkClick(View view){
-        Intent intent = getPackageManager().getLaunchIntentForPackage(Constants.WEWORK_PACKAGE_NAME);
-        startActivity(intent);
+        Log.d(TAG, "打开企业微信");
+        try {
+            openWework();
+            Toast.makeText(MainActivity.this, "打开企业微信", Toast.LENGTH_SHORT).show();
+        }catch (Exception ignored){
+            Toast.makeText(MainActivity.this, "启动企业微信失败", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    public void groupSendTask(View view){
+        Log.d(TAG, "执行1V1私聊群发");
+        try {
+            Toast.makeText(MainActivity.this, "执行群发助手任务", Toast.LENGTH_SHORT).show();
+        } catch (Exception ignored) {
+            Toast.makeText(MainActivity.this, "error..", Toast.LENGTH_SHORT).show();
+        }
+        openWework();
+    }
+
+    public void customerFriendCircleTask(View view){
+        Log.d(TAG, "执行客户朋友圈任务");
+        try {
+            Toast.makeText(MainActivity.this, "执行客户朋友圈任务", Toast.LENGTH_SHORT).show();
+        } catch (Exception ignored) {
+            Toast.makeText(MainActivity.this, "error..", Toast.LENGTH_SHORT).show();
+        }
+        openWework();
+    }
+
+    public void sopFriendCircleTask(View view){
+        Log.d(TAG, "执行sop朋友圈任务");
+        try {
+            Toast.makeText(MainActivity.this, "执行sop朋友圈任务", Toast.LENGTH_SHORT).show();
+            openWework();
+            if (this.jobScheduler == null) {
+                return;
+            }
+            this.jobScheduler.start();
+            this.jobScheduler.addJob(RobotJobType.SOP_AGENT_SEND_MOMENT);
+        } catch (Exception ignored) {
+            Toast.makeText(MainActivity.this, "error..", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void allTask(View view){
+        Log.d(TAG, "执行所有任务");
+        try {
+            Toast.makeText(MainActivity.this, "循环执行所有任务", Toast.LENGTH_SHORT).show();
+        } catch (Exception ignored) {
+            Toast.makeText(MainActivity.this, "error..", Toast.LENGTH_SHORT).show();
+        }
+        openWework();
+    }
+
 
     public void btnAccessSettingClick(View view){
-        startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+        Log.d(TAG, "跳转到辅助功能");
+        try {
+            startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+            Toast.makeText(MainActivity.this, "Accessibility service", Toast.LENGTH_SHORT).show();
+        } catch (Exception ignored) {
+            Toast.makeText(MainActivity.this, "can not open Accessibility service", Toast.LENGTH_SHORT).show();
+        }
     }
 
+    public void suspendedBallSetting(View view){
+        Log.d(TAG, "跳转到悬浮球功能");
+        try {
+            Toast.makeText(MainActivity.this, "check suspension-Window permission", Toast.LENGTH_SHORT).show();
+            if (!Settings.canDrawOverlays(this)) {
+                //没有权限，需要申请悬浮球权限
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 100);
+            } else {
+                //已经有权限，可以直接显示悬浮窗
+                Toast.makeText(MainActivity.this, "suspension-Window have accessed！", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception ignored) {
+            Toast.makeText(MainActivity.this, "check suspension-Window permission error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void suspensionWindowPermissionCheck() {
+        if (!Settings.canDrawOverlays(this)) {
+            //没有权限，需要申请悬浮球权限
+            Toast.makeText(MainActivity.this, "请点击检查悬浮球权限是否已打开", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     public void btnScheduleJobClick(View view){
+        Log.d(TAG, "定时任务开始");
         this.btnOpenWeWorkClick(view);
         this.scheduleJob();
     }
 
+    public void openWework(){
+        //打开企微
+        Intent intent = getPackageManager().getLaunchIntentForPackage(Constants.WEWORK_PACKAGE_NAME);
+        startActivity(intent);
+    }
+
     public void btnCancelScheduleJobClick(View view){
+        Log.d(TAG, "停止定时任务");
         this.cancelScheduleJob();
     }
 
--- app/src/main/java/com/scrm/robot/taskmanager/job/SopAgentSendMomentJob.java
+++ app/src/main/java/com/scrm/robot/taskmanager/job/SopAgentSendMomentJob.java
@@ -1,19 +1,31 @@
 package com.scrm.robot.taskmanager.job;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import com.scrm.robot.RobotApplication;
 import com.scrm.robot.taskmanager.RobotAccessibilityContext;
 import com.scrm.robot.taskmanager.enums.RobotRunState;
+import com.scrm.robot.utils.AccessibilityGestureUtil;
 import com.scrm.robot.utils.AccessibilityNodeFinder;
 import com.scrm.robot.utils.ApplicationUtil;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 public class SopAgentSendMomentJob extends BaseRobotJob {
     private final static String TAG = SopAgentSendMomentJob.class.getName();
+    public static String taskStatus = "START_SOP";
+    public static String targetTag = "转发文章";
+    public static List tagList;
+    public static List tempList;
+    public static int tempTagTimes = 0;
+    public AccessibilityGestureUtil accessibilityGestureUtil;
+    private final static String packageName="com.tencent.wework";
 
     public SopAgentSendMomentJob(){
         super();
@@ -24,6 +36,12 @@ public class SopAgentSendMomentJob extends BaseRobotJob {
         Log.d(TAG, String.format("%s start run", this.getJobId()));
         this.setJobState(RobotRunState.STARTED);
         this.setStartTime(new Date());
+    }
+
+    @Override
+    public void stop() {
+        Log.d(TAG, String.format("%s stop", this.getJobId()));
+        super.stop();
     }
 
     @Override
@@ -43,36 +61,398 @@ public class SopAgentSendMomentJob extends BaseRobotJob {
         if (rootNodeInfo == null) {
             return;
         }
-        // 搜索按钮
-        AccessibilityNodeInfo searchBtn = AccessibilityNodeFinder.findNodeByViewId(rootNodeInfo, "com.tencent.wework:id/kci");
-        if (searchBtn != null) {
-//            return;
-            searchBtn.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-        }
+        SopFriendCircle(rootNodeInfo);
+    }
 
-        // 搜索输入框
-        AccessibilityNodeInfo searchInput = AccessibilityNodeFinder.findNodeByViewId(rootNodeInfo, "com.tencent.wework:id/iqz");
-        if (searchInput != null) {
-            searchInput.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
+    public void SopFriendCircle(AccessibilityNodeInfo rootNodeInfo) {
+        switch (taskStatus) {
+            case "START_SOP":
+                sopTaskStart(rootNodeInfo);
+                break;
+            case "READY_TO_SHARE":
+                shareTask(rootNodeInfo);
+                break;
+            case "CONFIRM_CUSTOMER":
+                publishTask(rootNodeInfo);
+                break;
+            case "TAG_FOUND":
+                tagFound(rootNodeInfo);
+                break;
+            case "REPLY_SOP":
+                sopReplied(rootNodeInfo);
+                break;
+            case "BACK_TO_SOP_LIST_AND_DELETE":
+                backToSopListAndDelete(rootNodeInfo);
+                break;
+            case "SOP_DELETE":
+                sopDelete(rootNodeInfo);
+                break;
+            case "DELETE_CONFIRM":
+                confirmDelete(rootNodeInfo);
+                break;
+            case "BACK_TO_MAIN":
+                backToMain(rootNodeInfo);
+                break;
+        }
+    }
+
+    private void sopTaskStart(AccessibilityNodeInfo rootNodeInfo) {
+        //sop任务进入流程
+        uiSearch(rootNodeInfo);       //搜索
+        uiSearchLabel(rootNodeInfo);      //搜索框输入
+        searchResult(rootNodeInfo);       //搜索结果
+        sopClickIn(rootNodeInfo);         //sop任务页
+    }
+
+    private void shareTask(AccessibilityNodeInfo rootNodeInfo) {
+        //分享之后的流程
+        clickToShare(rootNodeInfo);       //分享
+        chooseVisibleCustomer(rootNodeInfo);      //选择可见的客户
+        choosePartialCustomer(rootNodeInfo);      //部分客户
+        tagFilter(rootNodeInfo);      //根据标签筛选
+        chooseTag(rootNodeInfo);      //寻找标签
+    }
+
+    private void publishTask(AccessibilityNodeInfo rootNodeInfo){
+        //发布流程
+        confirmCustomer(rootNodeInfo);    //全部客户选择完成->确定
+        _confirmCustomer(rootNodeInfo);   //部分可见->确定
+        publishFriendCircle(rootNodeInfo); //发表
+        friendCircleBack(rootNodeInfo);   //朋友圈返回
+    }
+
+    private void tagFound(AccessibilityNodeInfo rootNodeInfo) {
+        //找到了标签
+        System.out.println("标签已找到");
+        confirmTag(rootNodeInfo);     //标签页->确定
+        selectAllCustomer(rootNodeInfo);      //全选客户
+    }
+
+    public void performClick(AccessibilityNodeInfo targetInfo) {
+        //ui动作:点击
+        try {
+            targetInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }catch (Exception e){
+            System.out.println("点击失败");
+        }
+    }
+
+    public void performLongClick(AccessibilityNodeInfo targetInfo) {
+        //ui动作:长按点击
+        try {
+            targetInfo.performAction(AccessibilityNodeInfo.ACTION_LONG_CLICK);
+        }catch (Exception e){
+            System.out.println("点击失败");
+        }
+    }
+
+    public void performScroll(AccessibilityNodeInfo targetInfo) {
+        //ui动作:向下滑动
+        try {
+            targetInfo.performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
+        }catch (Exception e){
+            System.out.println("滑动失败");
+        }
+    }
+
+    public void inputText(AccessibilityNodeInfo targetInfo){
+        //输入text
+        try {
+            targetInfo.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
             Bundle arguments = new Bundle();
-            // TODO @zyh low app名称配置
-            arguments.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "SMR-代开发-test");
-            searchInput.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
-        }
-
-        // app点击
-        AccessibilityNodeInfo appViewGroup = AccessibilityNodeFinder.findNodeByViewId(rootNodeInfo, "com.tencent.wework:id/fyu");
-        if (appViewGroup != null) {
-            AccessibilityNodeInfo parent = appViewGroup.getParent();
-            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            this.stop();
+            arguments.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, ResourceId.testServer);
+            targetInfo.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
+        }catch (Exception e){
+            System.out.println("输入失败");
         }
     }
 
-
-    @Override
-    public void stop() {
-        Log.d(TAG, String.format("%s stop", this.getJobId()));
-        super.stop();
+    private void uiSearch(AccessibilityNodeInfo rootNodeInfo) {
+        //寻找->尝试点击搜索
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SEARCH);
+//        System.out.println("找到'搜索'ui数量"+targetUis.size());
+        if(targetUis.size() > 0){
+            System.out.println("点击搜索");
+            performClick(targetUis.get(0));
+        }
     }
+
+    private void uiSearchLabel(AccessibilityNodeInfo rootNodeInfo) {
+        //寻找->搜索框聚焦->输入
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SEARCH_LABEL);
+//        System.out.println("找到'搜索框'ui数量"+targetUis.size());
+        if(targetUis.size()>0){
+            System.out.println("输入:"+ResourceId.testServer);
+            inputText(targetUis.get(0));
+        }
+    }
+
+    private void searchResult(AccessibilityNodeInfo rootNodeInfo) {
+        //寻找->尝试点击搜索结果
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SEARCH_RESULT);
+        if(targetUis.size()>0){
+            System.out.println("找到结果:"+ResourceId.testServer);
+            performClick(targetUis.get(0).getParent());
+        }
+    }
+
+    private void sopClickIn(AccessibilityNodeInfo rootNodeInfo) {
+        //寻找->sop朋友圈消息->尝试点击进入
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SOP);
+        if(targetUis.size()>0){
+            Collections.reverse(targetUis);
+            performClick(targetUis.get(0));
+            sysSleep(7000);
+            this.accessibilityGestureUtil.click(360, 1550);
+            taskStatus = "READY_TO_SHARE";
+//            for(AccessibilityNodeInfo targetUi:targetUis){
+//                if(targetUi.getChildCount()>3){
+//                    System.out.println("点击进入一条SOP");
+//                    performClick(targetUi);
+//                    sysSleep(7000);
+//                    this.accessibilityGestureUtil.click(360, 1550);
+//                    taskStatus = "READY_TO_SHARE";
+//                    return;
+//                }
+//            }
+        }
+    }
+
+    private void sopDelete(AccessibilityNodeInfo rootNodeInfo){
+        //长按+删除
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SOP);
+        List<AccessibilityNodeInfo> deleteUis = rootNodeInfo.findAccessibilityNodeInfosByText("删除");
+        if(targetUis.size()>0) {
+            Collections.reverse(targetUis);
+            for (AccessibilityNodeInfo targetUi : targetUis) {
+                if (targetUi.getChildCount() > 3) {
+                    System.out.println("删除一条SOP");
+                    performLongClick(targetUis.get(0));
+                    if(deleteUis.size()>0){
+                        performClick(deleteUis.get(0).getParent());
+                        taskStatus = "DELETE_CONFIRM";
+                    }
+                }
+            }
+        }
+    }
+
+    private void confirmDelete(AccessibilityNodeInfo rootNodeInfo) {
+        //长按删除后的确定
+        List<AccessibilityNodeInfo> confirmUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_3);
+        if(confirmUis.size()>0){
+            performClick(confirmUis.get(0));
+            taskStatus = "START_SOP";
+        }
+    }
+
+    private void clickToShare(AccessibilityNodeInfo rootNodeInfo) {
+        //寻找->sop朋友圈消息->点击一键分享到朋友圈
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByText("一键分享朋友圈");
+        if(targetUis.size()>0){
+            System.out.println("分享朋友圈ui数量-"+targetUis.size());
+        }
+    }
+
+    private void chooseVisibleCustomer(AccessibilityNodeInfo rootNodeInfo){
+        //寻找->可见的客户并点击
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.VISIBLE_0);
+        List<AccessibilityNodeInfo> targetUis1 = rootNodeInfo.findAccessibilityNodeInfosByText("公开");
+        if(targetUis.size()>0 && targetUis1.size()>0){
+            System.out.println("点击可见的客户");
+            performClick(targetUis.get(0));
+        }
+    }
+
+    private void choosePartialCustomer(AccessibilityNodeInfo rootNodeInfo){
+        //寻找->点击部分可见
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.VISIBLE_1);
+        if(targetUis.size()>0 && targetUis.get(0).getChild(2).getChildCount()==0){
+            System.out.println("点击部分可见");
+            performClick(targetUis.get(0));
+        }
+    }
+
+    private void tagFilter(AccessibilityNodeInfo rootNodeInfo){
+        //寻找->点击根据标签筛选
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByText("根据标签筛选");
+        if(targetUis.size()>0){
+            System.out.println("点击部分可见");
+            performClick(targetUis.get(0).getParent().getParent());
+        }
+    }
+
+    private void chooseTag(AccessibilityNodeInfo rootNodeInfo){
+        //寻找->选择对应标签
+        List<AccessibilityNodeInfo> tagName = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.PERSONAL_TAG);
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByText(targetTag);
+//        System.out.println("标签组ui数量："+tagGroup.size());
+        try {
+            if(tagName.size()>0){
+                for(int i=0;i<tagName.size();i++){
+                    for(int k=0;k<tagName.get(i).getChildCount();k++){
+                        String tag = tagName.get(i).getChild(k).getText().toString();
+                        System.out.println("标签内容："+tag);
+                        if(tag.equals(targetTag)){
+                            performClick(tagName.get(i).getChild(k));
+                        }
+                    }
+                }
+                System.out.println("翻一整页");
+                performScroll(tagName.get(0).getParent().getParent().getParent().getParent().getParent());
+            }
+        }catch (Exception e){
+            Log.d(TAG,"选标签时出了点小错误："+e);
+        }
+    }
+
+    private void confirmTag(AccessibilityNodeInfo rootNodeInfo){
+        //确定标签
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_0);
+        if(targetUis.size()>0){
+            System.out.println("点击标签-确定");
+            performClick(targetUis.get(0));
+        }
+    }
+
+    private void selectAllCustomer(AccessibilityNodeInfo rootNodeInfo){
+        //全选客户
+        List<AccessibilityNodeInfo> allCustomer = rootNodeInfo.findAccessibilityNodeInfosByText("全部客户");
+        List<AccessibilityNodeInfo> confirm = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_1);
+        if(allCustomer.size()>0 && confirm.size()>0){
+            System.out.println("点击'全部客户'");
+            performClick(allCustomer.get(0).getParent().getParent().getParent().getParent().getParent().getParent());
+            taskStatus = "CONFIRM_CUSTOMER";
+        }else {
+            Log.d(TAG,"找不到标签对应的客户");
+            taskStatus = "REPLY_SOP";
+        }
+    }
+
+    private void confirmCustomer(AccessibilityNodeInfo rootNodeInfo){
+        //全选客户后点击
+        List<AccessibilityNodeInfo> confirmUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_1);
+        if(confirmUis.size()>0){
+            System.out.println("发现确定按钮："+confirmUis.get(0).getText().toString());
+            if(!confirmUis.get(0).getText().toString().equals("确定")){
+                System.out.println("点击'确定'");
+                performClick(confirmUis.get(0));
+            }else {
+                taskStatus = "TAG_FOUND";
+            }
+        }
+    }
+
+    private void _confirmCustomer(AccessibilityNodeInfo rootNodeInfo){
+        //部分可见->确定
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.VISIBLE_1);
+        List<AccessibilityNodeInfo> confirmUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_2);
+        if(targetUis.size()>0 && targetUis.get(0).getChild(2).getChildCount()!=0 && confirmUis.size()>0){
+            System.out.println("点击'确定'");
+            performClick(confirmUis.get(0));
+        }
+    }
+
+    private void publishFriendCircle(AccessibilityNodeInfo rootNodeInfo){
+        //发表
+        List<AccessibilityNodeInfo> publishUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.PUBLISH);
+        if(publishUis.size()>0){
+            System.out.println("点击发表");
+            performClick(publishUis.get(0));
+        }
+    }
+
+    private void friendCircleBack(AccessibilityNodeInfo rootNodeInfo){
+        //跳转到朋友圈界面就返回
+        List<AccessibilityNodeInfo> pyqUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.PYQ);
+        List<AccessibilityNodeInfo> backUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.BACK);
+        if(pyqUis.size()>0 && backUis.size()>0){
+            System.out.println("点击'返回'");
+            performClick(backUis.get(0));
+            taskStatus = "REPLY_SOP";
+        }
+    }
+
+    private void sopReplied(AccessibilityNodeInfo rootNodeInfo){
+        //点击回执
+        List<AccessibilityNodeInfo> pyqUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.PAGE_TITLE);
+        List<AccessibilityNodeInfo> backUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.BACK);
+        List<AccessibilityNodeInfo> confirmUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_4);
+        if(pyqUis.size()>0 && pyqUis.get(0).getText().toString().equals("朋友圈")){
+            System.out.println("点击'回执'");
+            this.accessibilityGestureUtil.click(620, 1350);
+            sysSleep(1000);
+            taskStatus = "BACK_TO_SOP_LIST_AND_DELETE";
+        }else{
+            System.out.println("点击'返回'");
+            if(backUis.size()>0){
+                performClick(backUis.get(0));
+            }
+            if(confirmUis.size()>0) {
+                performClick(confirmUis.get(0));
+            }
+        }
+    }
+
+    public void backToMain(AccessibilityNodeInfo rootNodeInfo) {
+        //返回主界面
+        List<AccessibilityNodeInfo> userUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.USER);
+        List<AccessibilityNodeInfo> chatUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CHAT);
+        List<AccessibilityNodeInfo> backUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.BACK);
+        List<AccessibilityNodeInfo> confirmUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_4);
+        if (userUis.size()>0 && chatUis.size()>0){
+            // TODO 任务完成
+        }else {
+            if(backUis.size()>0){
+                performClick(backUis.get(0));
+            }
+            if(confirmUis.size()>0){
+                performClick(confirmUis.get(0));
+            }
+        }
+    }
+
+    public void backToSopList(AccessibilityNodeInfo rootNodeInfo) {
+        //返回到SOP列表
+        List<AccessibilityNodeInfo> sopTitle = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.PAGE_TITLE);
+        List<AccessibilityNodeInfo> backUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.BACK);
+        List<AccessibilityNodeInfo> confirmUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_4);
+        if (sopTitle.size()>0 && sopTitle.get(0).getText().toString().equals(ResourceId.testServer)){
+        }else {
+            if(backUis.size()>0){
+                performClick(backUis.get(0));
+            }
+            if(confirmUis.size()>0){
+                performClick(confirmUis.get(0));
+            }
+        }
+    }
+
+    public void backToSopListAndDelete(AccessibilityNodeInfo rootNodeInfo) {
+        //返回到SOP列表
+        List<AccessibilityNodeInfo> sopTitle = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.PAGE_TITLE);
+        List<AccessibilityNodeInfo> backUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.BACK);
+        List<AccessibilityNodeInfo> confirmUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CONFIRM_4);
+        if (sopTitle.size()>0 && sopTitle.get(0).getText().toString().equals(ResourceId.testServer)){
+            taskStatus = "SOP_DELETE";
+        }else {
+            if(backUis.size()>0){
+                performClick(backUis.get(0));
+            }
+            if(confirmUis.size()>0){
+                performClick(confirmUis.get(0));
+            }
+        }
+    }
+
+    private void sysSleep(int msecond) {
+        //睡眠 param:seconds
+        try {
+            System.out.println("睡眠一秒");
+            Thread.sleep(msecond);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
