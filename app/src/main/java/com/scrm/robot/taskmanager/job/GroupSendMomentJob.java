d759f0dab9dad7c9a3d46b780cc125da119e9d74
zhangyuhang <2657955369@qq.com>
2022-03-18 16:56:22 +0800

    feat:finish GROUP-SEND task

--- app/src/main/java/com/scrm/robot/taskmanager/job/GroupSendMomentJob.java
+++ app/src/main/java/com/scrm/robot/taskmanager/job/GroupSendMomentJob.java
@@ -5,7 +5,6 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import com.scrm.robot.RobotApplication;
-import com.scrm.robot.taskmanager.JobStateViewModel;
 import com.scrm.robot.taskmanager.RobotAccessibilityContext;
 import com.scrm.robot.taskmanager.enums.RobotRunState;
 import com.scrm.robot.utils.AccessibilityGestureUtil;
@@ -18,11 +17,11 @@ public class GroupSendMomentJob  extends BaseRobotJob {
     private final static String TAG = GroupSendMomentJob.class.getName();
     public AccessibilityGestureUtil accessibilityGestureUtil;
     private final static String packageName="com.tencent.wework";
+    private static boolean afterClickGroupSend=false;
 
     public GroupSendMomentJob(){
         super();
         this.setTaskStatus("TASK_START");
-        RobotApplication robotApplication = (RobotApplication) ApplicationUtil.getApplication();
     }
 
     @Override
@@ -47,13 +46,14 @@ public class GroupSendMomentJob  extends BaseRobotJob {
         Log.d(TAG, String.format("%s processing", this.getJobId()));
         RobotApplication application = (RobotApplication) ApplicationUtil.getApplication();
         RobotAccessibilityContext robotAccessibilityContext = application.getRobotAccessibilityContext();
+        AccessibilityEvent currentEvent = robotAccessibilityContext.getCurrentEvent();
+        afterClickGroupSend = currentEvent.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
 
         this.accessibilityGestureUtil=new AccessibilityGestureUtil(robotAccessibilityContext.getWeWorkAccessibilityService());
         AccessibilityNodeInfo rootNodeInfo = robotAccessibilityContext.getRootNodeInfo();
         if (rootNodeInfo == null) {
             return;
         }
-        // TODO 群发任务主流程
         groupSendTask(rootNodeInfo);
     }
 
@@ -62,18 +62,61 @@ public class GroupSendMomentJob  extends BaseRobotJob {
             case "TASK_START":
                 findGroupSendHelper(rootNodeInfo);
                 break;
+            case "GROUP_HELPER":
+                _findGroupSendHelper(rootNodeInfo);
+                break;
+            case "FIND_MSG":
+                checkGroupSendMsg(rootNodeInfo);
+                break;
+            case "TODO_SEND":
+                sendMsg(rootNodeInfo);
+                break;
         }
     }
 
     private void findGroupSendHelper(AccessibilityNodeInfo rootNodeInfo){
-        //寻找->尝试点击搜索
-        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SEARCH);
-//        System.out.println("找到'搜索'ui数量"+targetUis.size());
+        //寻找->点击工作台
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.CHAT);
         if(targetUis.size() > 0){
-            System.out.println("点击搜索");
-            performClick(targetUis.get(0));
-            if(!JobStateViewModel.isScreenShot.getValue()){
-                JobStateViewModel.isScreenShot.postValue(true); }
+            System.out.println("点击工作台");
+            performClick(targetUis.get(0).getChild(3));
+            this.setTaskStatus("GROUP_HELPER");
+        }
+    }
+
+    private void _findGroupSendHelper(AccessibilityNodeInfo rootNodeInfo){
+        //寻找->点击群发助手
+        List<AccessibilityNodeInfo> groupSendUis = rootNodeInfo.findAccessibilityNodeInfosByText("群发助手");
+        if(groupSendUis.size() > 0){
+            System.out.println("点击群发助手");
+            performClick(groupSendUis.get(0).getParent().getParent());
+            this.setTaskStatus("FIND_MSG");
+        }
+    }
+
+    private void checkGroupSendMsg(AccessibilityNodeInfo rootNodeInfo){
+        //检测有无待发消息
+        if(afterClickGroupSend){
+            List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SEND_FLAG);
+            if(targetUis.size() > 0){
+                System.out.println("点击待发送入口");
+                performClick(targetUis.get(0));
+                this.setTaskStatus("TODO_SEND");
+            }else {
+                System.out.println("当前没有待发送消息");
+                this.setTaskStatus("NO_MSG");
+            }
+        }
+    }
+
+    private void sendMsg(AccessibilityNodeInfo rootNodeInfo){
+        //处理待发送消息
+        List<AccessibilityNodeInfo> targetUis = rootNodeInfo.findAccessibilityNodeInfosByViewId(ResourceId.SEND_PAGE);
+        if(targetUis.size() > 0){
+            String time = targetUis.get(0).getChild(0).getChild(0).getChild(1).getText().toString();
+            //TODO time限制
+            //点击发送
+            performClick(targetUis.get(0).getChild(0).getChild(2));
         }
     }
 
--- app/src/main/java/com/scrm/robot/taskmanager/job/SopAgentSendMomentJob.java
+++ app/src/main/java/com/scrm/robot/taskmanager/job/SopAgentSendMomentJob.java
@@ -43,7 +43,6 @@ public class SopAgentSendMomentJob extends BaseRobotJob {
     public SopAgentSendMomentJob(){
         super();
         this.setTaskStatus("START_SOP");
-        RobotApplication robotApplication = (RobotApplication) ApplicationUtil.getApplication();
     }
 
     @Override
